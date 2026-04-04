dd96b032a01124a8598f340fc7b13cf188da8294
teo2823 <mateo2823@hotmail.com>
2026-04-03 20:27:19 -0500

    feat: add icon and splash screen

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_native_splash/flutter_native_splash.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:toastification/toastification.dart';
 
@@ -7,6 +8,8 @@ import 'features/home/home_shell.dart';
 import 'providers/theme_provider.dart';
 
 void main() {
+  final binding = WidgetsFlutterBinding.ensureInitialized();
+  FlutterNativeSplash.preserve(widgetsBinding: binding);
   runApp(const ProviderScope(child: App()));
 }
 
